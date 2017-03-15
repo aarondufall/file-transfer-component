c6578436335cf44d0fc955bb82608e575799e43f
aarondufall <aald212@gmail.com>
2017-03-15 15:09:02 -0700

    Start svc with evt host and consumer

---
+++ lib/file_transfer_component/consumers/event.rb
@@ -0,0 +1,17 @@
+module FileTransferComponent
+  module Consumers
+    class Event
+      include Consumer
+
+      # Specifies handler implementations for handling messages
+      handler Handlers::Events::Initiated
+
+      # Errors are handled by this method. If omitted, the default action when an
+      # error is raised during the dispatching of a message is to re-raise the error
+      def error_raised(error, event_data)
+        #SomeErrorNotificationService.(error)
+        #WriteNotProcessable.(event_data)
+      end
+    end
+  end
+end
